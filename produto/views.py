f7aed80583a8a809125494b8a7ba146fd110ba3f
Marcos Tromel <marcos.tromel@gmail.com>
2025-01-10 21:00:06 -0300

    corrigido resumo da compra user sem perfil e carrinho sem produto

--- produto/views.py
+++ produto/views.py
@@ -5,6 +5,7 @@ from django.views import View
 # from django.http import HttpResponse
 from django.contrib import messages
 from . import models
+from perfil.models import Perfil
 
 
 class ListaProdutos(ListView):
@@ -23,10 +24,6 @@ class DetalheProduto(DetailView):
 
 class AdicionaAoCarrinho(View):
     def get(self, *args, **kwargs):
-        # TODO: Remover linhas abaixo
-        # if self.request.session.get('carrinho'):
-        #     del self.request.session['carrinho']
-        #     self.request.session.save()
 
         http_referer = self.request.META.get(
             'HTTP_REFERER',
@@ -160,6 +157,22 @@ class ResumoDaCompra(View):
     def get(self, *args, **kwargs):
         if not self.request.user.is_authenticated:
             return redirect('perfil:criar')
+
+        perfil = Perfil.objects.filter(usuario=self.request.user).exists()
+
+        if not perfil:
+            messages.error(
+                self.request,
+                'Usuário sem perfil.'
+            )
+            return redirect('perfil:criar')
+
+        if not self.request.session.get('carrinho'):
+            messages.error(
+                self.request,
+                'Carrinho vazio.'
+            )
+            return redirect('produto:lista')
 
         contexto = {
             'usuario': self.request.user,
